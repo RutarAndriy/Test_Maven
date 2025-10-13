3e62e340ea14471bc2f72e79d4933c3a4637d0ad
Rutar Andriy <RutarAndriy@gmail.com>
2025-10-13 09:18:19 +0300

    Test build

---
+++ src/main/java/com/rutar/mvn_test/MVN_Test.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rutar.mvn_test;
+
+/**
+ *
+ * @author Rutar
+ */
+public class MVN_Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
